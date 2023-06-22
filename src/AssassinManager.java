1bfbc21cde6eee74b1d5d2adf601097d68c98910
Gabe9720 <gabreonpreston.womack@calbaptist.edu>
2023-06-21 20:16:39 -0700

    First and last commit

--- src/AssassinManager.java
+++ src/AssassinManager.java
@@ -2,39 +2,40 @@ import java.util.List;
 
 public class AssassinManager {
     //fields:
+//aliveFront - the front of the kill ring
+//graveyardFront - the front of the list of assassins who were eliminated
+    private AssassinNode aliveFront;
+    private AssassinNode graveyardFront;    //fields:
     //aliveFront- the aliveFront of the kill ring
     //graveyardFront- aliveFront of the list of the assassins who were eliminated
-    private AssassinNode aliveFront;
-    private AssassinNode graveyardFront;
 
-    //pre: construct an AssassinManager object from a given list of strings
-    //post: creates a linkedlist of assassin nodes from names in the list
-    //throws exception if the list is null or empty
-    public AssassinManager(List<String> names){
-        if(names == null || names.isEmpty())
+    //pre: Construct an AssassinManager object from a given list of strings
+//post: Creates a linked list of assassin nodes from names in the list
+//throws an exception if the list is null or empty
+    public AssassinManager(List<String> names) {
+        if (names == null || names.isEmpty())
             throw new IllegalArgumentException("List may not be null or empty.");
 
-        //initialize graveyard list lead by graveyardFront
+        //initialize graveyard list led by graveyardFront
         graveyardFront = null;
 
         //initialize alive list reading backward
         aliveFront = null;
-        for (int i = names.size() - 1 ; i >= 0 ; i--) {
+        for (int i = names.size() - 1; i >= 0; i--) {
             aliveFront = new AssassinNode(names.get(i), aliveFront);
         }
     }
 
-    //pre: print the kill ring
-    //post: prints kill ring; if only one person is in the kill ring, that person wins
-    public void printKillRing(){
-        if(isGameOver()){
+    //pre: Print the kill ring
+//post: Prints the kill ring; if only one person is in the kill ring, that person wins
+    public void printKillRing() {
+        if (isGameOver()) {
             System.out.println("    " + aliveFront.name + " won the game!");
-        }
-        else{
+        } else {
             AssassinNode current = aliveFront;
-            while(current != null){
+            while (current != null) {
                 System.out.println(current);
-                if(current.next == null) {
+                if (current.next == null) {
                     System.out.println(aliveFront.name);
                 }
                 current = current.next;
@@ -42,31 +43,31 @@ public class AssassinManager {
         }
     }
 
-    //pre: print out graveyard
-    //post: prints graveyard along with their killer; if graveyard is empty nothing happens
-    public void printGraveyard(){
+    //pre: Print out the graveyard
+//post: Prints the graveyard along with their killer; if the graveyard is empty, nothing happens
+    public void printGraveyard() {
         AssassinNode current = graveyardFront;
-        while(current != null){
+        while (current != null) {
             System.out.println("    " + current.name + " was killed by " + current.killer);
             current = current.next;
         }
     }
 
-    //pre: determine if game is over
-    //post: returns true if there is only one person in the kill ring; otherwise false
-    public boolean isGameOver(){
+    //pre: Determine if the game is over
+//post: Returns true if there is only one person in the kill ring; otherwise, returns false
+    public boolean isGameOver() {
         return (aliveFront.next == null);
     }
 
     //helper method
-    //param: name being searched for and give the starting node
-    //to indicate to search the graveyard or kill ring
-    //pre: determine if one of the linkedlists contains the desired name
-    //post: returns true if name is in the linkedlist; otherwise false
-    private boolean containsHelper(String name, AssassinNode start){
+//param: The name being searched for and the starting node
+//to indicate whether to search the graveyard or the kill ring
+//pre: Determine if one of the linked lists contains the desired name
+//post: Returns true if the name is in the linked list; otherwise, returns false
+    private boolean containsHelper(String name, AssassinNode start) {
         AssassinNode current = start;
-        while(current != null){
-            if(current.name.equalsIgnoreCase(name)){
+        while (current != null) {
+            if (current.name.equalsIgnoreCase(name)) {
                 return true;
             }
             current = current.next;
@@ -74,51 +75,49 @@ public class AssassinManager {
         return false;
     }
 
-    //pre: check if a name is in the kill ring
-    //post: returns true if name is found; otherwise false
-    public boolean killRingContains(String name){
+    //pre: Check if a name is in the kill ring
+//post: Returns true if the name is found; otherwise, returns false
+    public boolean killRingContains(String name) {
         return containsHelper(name, aliveFront);
     }
 
-    //pre: check if a name is in the graveyard
-    //post: returns true if name is found; otherwise false
-    public boolean graveyardContains(String name){
+    //pre: Check if a name is in the graveyard
+//post: Returns true if the name is found; otherwise, returns false
+    public boolean graveyardContains(String name) {
         return containsHelper(name, graveyardFront);
     }
 
-    //pre: check if there is a winner
-    //post: returns the name in the aliveFront of the linked list if the game is over; else false
-    public String winner(){
-        if(!isGameOver())
+    //pre: Check if there is a winner
+//post: Returns the name in the front of the alive list if the game is over; otherwise, returns null
+    public String winner() {
+        if (!isGameOver())
             return null;
         return aliveFront.name;
     }
 
-    //param: name of person to be killed
-    //pre: kill the name of the person passed
-    //post: removes name of the person from the kill list to the graveyard;
-    //exception is thrown if the game is over or if the kill ring does not contain the given name
-    public void kill(String name){
-        if(isGameOver()){
+    //param: The name of the person to be killed
+//pre: Kill the person with the given name
+//post: Removes the name of the person from the kill list to the graveyard;
+//throws an exception if the game is over or if the kill ring does not contain the given name
+    public void kill(String name) {
+        if (isGameOver()) {
             throw new IllegalStateException("The game is over.");
-        }
-        else if(!killRingContains(name)){
+        } else if (!killRingContains(name)) {
             throw new IllegalArgumentException("This name is not in the kill ring.");
-        }
-        else{
+        } else {
             AssassinNode current = aliveFront;
-            //if name is in aliveFront
-            if(current.name.equalsIgnoreCase(name)){
+            //if the name is in aliveFront
+            if (current.name.equalsIgnoreCase(name)) {
                 aliveFront = aliveFront.next;
                 current.next = graveyardFront;
                 graveyardFront = current;
                 current = aliveFront;
-                while(current.next != null){
+                while (current.next != null) {
                     current = current.next;
                 }
                 graveyardFront.killer = current.name;
             }
-            //if name is in the middle
+            //if the name is in the middle
             else {
                 while (current.next != null) {
                     if (current.next.name.equalsIgnoreCase(name)) {
@@ -134,7 +133,8 @@ public class AssassinManager {
             }
         }
     }
-    //////// DO NOT MODIFY AssassinNode.  You will lose points if you do. ////////
+//////// DO NOT MODIFY AssassinNode.  You will lose points if you do. ////////
+
     /**
      * Each AssassinNode object represents a single node in a linked list
      * for a game of Assassin.
